f8fa590d87fd6d453a8f1b8e21d283c81771e45c
fgorordo <flgorordo@gmail.com>
2022-04-20 20:03:21 -0300

    heroku

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const publicPath = path.resolve(__dirname, './public')
 app.use(express.static(publicPath))
 
 // Server start
-app.listen(process.env.expressPort || expressPort, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Servidor iniciado en el puerto: ${expressPort}`)
 })
 
